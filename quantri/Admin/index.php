81b01d69a34ba8eeaa5e164ed16b3fcb06405fe4
TrongHiuuu <nthieu.c4pbchau2122@gmail.com>
2024-04-13 18:37:33 +0700

    .

---
+++ quantri/Admin/index.php
@@ -0,0 +1,52 @@
+<?php
+//not include controller
+include '../../config/config.php';
+include '../../lib/session.php';
+include '../../lib/connect.php';
+require '../model/user.php';
+
+$aside = "";
+$quyentaikhoan = "Admin";
+if(isset($_GET['page'])&&($_GET['page']!=="")){
+    switch(trim($_GET['page'])){
+        case 'home':
+            $result = getAllUser();
+            $pageTitle = "user";
+            require_once '../view/user.php';
+            break;
+
+        case 'searchUser':
+            $pageTitle = "searchUser";
+            if(isset($_POST['admin-controller-user'])){
+                require_once '../controller/filterUser.php';
+            }
+            else $result = $_SESSION['searchResult'];
+            require_once '../view/user.php';
+            break;
+        
+        case 'editInfo':
+            require_once "../controller/editInfo.php";
+            break;
+
+        case 'signOut':
+            admin_login_session_unset();
+            header("Location:../index.php?page=home");
+            break;
+
+        default:
+        //require homepage
+        $result = getAllUser();
+        $pageTitle = "user";
+        require_once '../view/user.php';
+        break;
+    }
+}
+else{
+    //require homepage
+    $result = getAllUser();
+    $pageTitle = "user";
+    require_once '../view/user.php';
+}
+
+    
+?>
